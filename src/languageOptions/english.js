e6a6d05f36a21f5f3c1c54fb95d99acc409a3de1
qjoshua <quesadaleonjoshua@gmail.com>
2022-06-18 17:03:50 -0600

    Add extra instructions

--- src/languageOptions/english.js
+++ src/languageOptions/english.js
@@ -17,11 +17,13 @@ export const englishClearButtonOptions = {
 export const englishInstructionsTitles = {
     0: "Select a language",
     1: "Welcome to Pathfinder!",
-    2: "Investigate!"
+    2: "What is Pathfinder?",
+    3: "Investigate!"
 }
 
 export const englishInstructions = {
     1: "To begin, choose an algorithm and a speed. You can also add your own walls or select the maze generation option. Finally, select a starting and ending point. When you are ready, press the start button to visualize the algorithm. By default, the starting algorithm is DFS with a fast speed.",
-    2: "You can do some research about the algorithms DFS, also known as Depth First Search; and BFS, known as Breadth First Search in the following link:",
-    3: "https://www.geeksforgeeks.org/difference-between-bfs-and-dfs/"
+    2: "Pathfinder allows you to visualize algorithms such as DFS and BFS. It is important to know that DFS does not necessarily find the shortest path, while BFS does.",
+    3: "You can do some research about the algorithms DFS, also known as Depth First Search; and BFS, known as Breadth First Search in the following link:",
+    4: "https://www.geeksforgeeks.org/difference-between-bfs-and-dfs/"
 }
--- src/languageOptions/spanish.js
+++ src/languageOptions/spanish.js
@@ -17,11 +17,13 @@ export const spanishClearButtonOptions = {
 export const spanishInstructionsTitles = {
     0: "Elija un idioma",
     1: "¡Bienvenido a Pathfinder!",
-    2: "¡Tiempo de investigar!"
+    2: "¿Qué hace Pathfinder?",
+    3: "¡Tiempo de investigar!"
 }
 
 export const spanishInstructions = {
     1: "Para empezar, selecciona un algoritmo y una velocidad. Además, puedes añadir tus propias paredes con el click derecho o seleccionar la opción de generar un laberinto. Finalmente, selecciona un punto de inicio y un punto de destino. Cuando estés listo, presiona el botón de iniciar para comenzar el algoritmo. Por defecto, el algoritmo inicial es DFS con una velocidad de rápido.",
-    2: "Puedes investigar sobre los algoritmos DFS, también conocido como Búsqueda en Profundidad; y BFS, conocido como Búsqueda en anchura en el siguiente enlace:",
-    3: "https://www.encora.com/es/blog/dfs-vs-bfs"
+    2: "Pathfinder permite visualizar algoritmos de búsqueda de caminos, como DFS y BFS. Es importante saber que DFS no necesariamente encuentra el camino más corto, mientras que BFS sí.",
+    3: "Puedes investigar sobre los algoritmos DFS, también conocido como Búsqueda en Profundidad; y BFS, conocido como Búsqueda en anchura en el siguiente enlace:",
+    4: "https://www.encora.com/es/blog/dfs-vs-bfs"
 }
